624bf97c7f5ee0807b92937f20fc7aa8c6efa989
niposch <niposch@gmail.com>
2026-01-17 23:40:32 +0100

    set up frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,25 @@
+import path from "path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from '@tailwindcss/vite' // <--- Add this
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // <--- Add this
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+})
